b68244797081acd99dd6f5870c4ad2e09283c4d2
Justin Anthony <justincanthony@gmail.com>
2021-07-07 00:59:13 -0600

    Add return current card method to rounds class

--- test/Rounds-test.js
+++ test/Rounds-test.js
@@ -15,4 +15,23 @@ describe('Round', function() {
 
     expect(round.deck).to.deep.equal([card1, card2, card3]);
   })
+  it('should be able to return the current card in play', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    round.returnCurrentCard();
+
+    expect(round.returnCurrentCard()).to.deep.equal(card1);
+  })
+  // it('should be able to track turns')
+  // it('should be able to store an array of incorrect guesses by card number')
+  // it('should be able to take a turn and and increase the turn count')
+  // it('should')
+  // it('should')
+  // it('should')
+  // it('should')
 })
