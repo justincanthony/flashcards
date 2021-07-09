5726450109e9dc8290b27d7307a4519033b69a28
Justin Anthony <justincanthony@gmail.com>
2021-07-08 20:55:35 -0600

    Update to es6 functions and use beforeeach

--- test/Game-test.js
+++ test/Game-test.js
@@ -4,17 +4,17 @@ const Round = require('../src/Rounds');
 const Game = require('../src/Game');
 
 describe('Game', () => {
-  let game
+  let game;
 
   beforeEach(() => {
     game = new Game();
-  })
+  });
 
   it('should be a function', () => {
     expect(Game).to.be.a('function');
-  })
+  });
   it('should be able to start a game with a new round', () => {
     game.start();
     expect(game.currentRound).to.be.an.instanceOf(Round);
-  })
-})
+  });
+});
