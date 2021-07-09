718b38a2218648f38eae257e4707abb79ce46f2e
Justin Anthony <justincanthony@gmail.com>
2021-07-08 20:54:26 -0600

    Refactor and edit styling

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ class Card {
     this.cardNum = cardNum;
     this.question = question;
     this.answers = answers;
-    this.correctAnswer = correctAnswer
+    this.correctAnswer = correctAnswer;
   }
 }
 module.exports = Card;
